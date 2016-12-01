c9fda63b2c1c32b72608863e4ecf58b5dc87c92e
apple <apple@quange.local>
2016-12-01 19:22:07 +0800

    初始化

---
+++ 666/666/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  666
+//
+//  Created by apple on 16/12/1.
+//  Copyright © 2016年 zhangbaiquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
